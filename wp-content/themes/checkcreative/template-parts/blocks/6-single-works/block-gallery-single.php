ac8e2815329abe45b5849fc772c77821080cdb9c
Paula Sanz <paula@sanz.eu>
2025-11-05 17:22:26 +0100

    fix: gallery allow video

--- wp-content/themes/checkcreative/template-parts/blocks/6-single-works/block-gallery-single.php
+++ wp-content/themes/checkcreative/template-parts/blocks/6-single-works/block-gallery-single.php
@@ -1,21 +1,35 @@
 <?php
-$images = get_field('block_project_gallery_repeater');
-if ($images) : ?>
+$items = get_field('block_project_gallery_items');
+if ($items) : ?>
 	<section class="block-single-gallery container position-relative py-5">
 		<div class="gallery-slider__viewport position-relative vw-100">
 			<div class="gallery-slider__track d-flex align-items-stretch justify-content-en gap-3" data-slider-track>
-				<?php foreach ($images as $image) {
-					$url = $image['block_project_gallery_repeater_image'] ? $image['block_project_gallery_repeater_image']['url'] : '';
-					$alt = $image['block_project_gallery_repeater_image'] ? $image['block_project_gallery_repeater_image']['alt'] : '';
+				<?php foreach ($items as $item) :
+					$image = isset($item['block_project_gallery_items_img']) ? $item['block_project_gallery_items_img'] : null;
+					$video = isset($item['block_project_gallery_items_video']) ? $item['block_project_gallery_items_video'] : null;
 				?>
 					<article class="gallery-slider__slide">
-						<img
-							src="<?php echo esc_url($url); ?>"
-							alt="<?php echo esc_attr($alt); ?>"
-							class="gallery-slider__image pointer-events-none"
-							loading="lazy">
+						<?php if (!empty($video['url'])) :
+							$video_type = !empty($video['mime_type']) ? $video['mime_type'] : 'video/mp4'; ?>
+							<video
+								class="gallery-slider__video pointer-events-none"
+								playsinline
+								muted
+								loop
+								autoplay>
+								<source src="<?php echo esc_url($video['url']); ?>" type="<?php echo esc_attr($video_type); ?>">
+								Tu navegador no soporta video HTML5.
+							</video>
+						<?php elseif (!empty($image['url'])) :
+							$alt = !empty($image['alt']) ? $image['alt'] : ''; ?>
+							<img
+								src="<?php echo esc_url($image['url']); ?>"
+								alt="<?php echo esc_attr($alt); ?>"
+								class="gallery-slider__image pointer-events-none"
+								loading="lazy">
+						<?php endif; ?>
 					</article>
-				<?php } ?>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</section>
