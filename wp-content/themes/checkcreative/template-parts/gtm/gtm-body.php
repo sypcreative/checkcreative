84d7875f72d65173a7ee082d0f84e944f1ff246e
Paula Sanz <paula@sanz.eu>
2025-11-13 12:14:40 +0100

    fix: deleted unnecessary stuff

--- wp-content/themes/checkcreative/template-parts/gtm/gtm-body.php
+++
@@ -1,4 +0,0 @@
-<noscript id="gtm-body">
-	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5ZJMWXJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<?php
